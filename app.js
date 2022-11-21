02bfed410d70ccb1385717c5776c30a0ca135d98
Nick <ntoukas.nick@gmail.com>
2022-11-20 21:26:05 -0500

    submit functionality started

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-// ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submit = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-// edit option
+let editElement;
+let editFlag = false;
+let editID = '';
 
-// ****** EVENT LISTENERS **********
+form.addEventListener('submit', addItem);
 
-// ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
 
-// ****** LOCAL STORAGE **********
-
-// ****** SETUP ITEMS **********
+  if (value && editFlag === false) {
+  } else if (value && editFlag) {
+  } else {
+  }
+}
